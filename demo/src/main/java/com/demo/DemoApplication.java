eb4b5693f71c8545a4e367137344646d88d3e380
Mohan Naidu <mohannaidu9900365129@gmail.com>
2022-08-03 11:23:00 +0530

    First commit

---
+++ demo/src/main/java/com/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@OpenAPIDefinition
+@SpringBootApplication
+@ComponentScan
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		
+		System.out.println("hello world!");
+	}
+	
+
+}
